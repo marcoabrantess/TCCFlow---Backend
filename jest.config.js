f816ffd47490c600f858ecfe0072a0630a1a2f09
Marco Aurélio <marco.abrantes@ufv.br>
2024-12-01 13:48:47 -0300

    configurações do jest

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+    setupFilesAfterEnv: ['./jest.setup.ts'],
 };
